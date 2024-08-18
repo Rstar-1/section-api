7d9303530b54c33588460a5237ff825771943ce9
Rstar-1 <rajshetye.5855@gmail.com>
2024-08-18 12:35:16 +0530

    bf

---
+++ models/website/PageforthSchema.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const pforthSchema = new mongoose.Schema(
+  {
+    sectionpath: {
+      type: String,
+      required: true,
+    },
+    sectionname: {
+      type: String,
+      required: true,
+    },
+    picture: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// model pforth
+const pforth = new mongoose.model("pforth", pforthSchema);
+module.exports = pforth;
---
+++ models/website/PagesecondShema.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const psecondSchema = new mongoose.Schema(
+  {
+    sectionpath: {
+      type: String,
+      required: true,
+    },
+    sectionname: {
+      type: String,
+      required: true,
+    },
+    picture: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// model psecond
+const psecond = new mongoose.model("psecond", psecondSchema);
+module.exports = psecond;
