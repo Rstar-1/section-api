7b212c7ae75ef0e6694d065bffa3f147ec25ed6e
Rstar-1 <rajshetye.5855@gmail.com>
2024-08-18 12:52:03 +0530

    h

---
+++ models/common/BannerSchema.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const bannerSchema = new mongoose.Schema(
+  {
+    sectionpath: {
+      type: String,
+      required: true,
+    },
+    sectionname: {
+      type: String,
+      required: true,
+    },
+    picture: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// model banner
+const banner = new mongoose.model("banner", bannerSchema);
+module.exports = banner;
